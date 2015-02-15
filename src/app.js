fc5ff4e392301f761bb534780f9235bf8728f410
Steve Groves <stevegroves123@gmail.com>
2015-02-15 14:31:32 +0000

    Fixed web error
    Fixed the error of missing ajax initialisation and added the up / down control.
    
    Need to centralise the servo by using the select button on the watch.

--- src/app.js
+++ src/app.js
@@ -1,40 +1,51 @@
-/**
- * Welcome to Pebble.js!
- */
-
 var UI = require('ui');
-var URLup = 'https://www.google.co.uk/?gws_rd=ssl';
-
+var ajax = require('ajax');
 var main = new UI.Card({
   title: 'Servo.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Servo World!',
-  body: 'Press any button to control the Servo'
+  body: '\nPress any button'
 });
 main.show();
 
 main.on('click', 'up', function(e) {
-ajax(
-  {
-    url: URLup,
-    method: 'get',
-    type: 'text'
-  },
-  function(success) {
-    // Success!
-    var return_page = new UI.Card ({
-      title:'result ',
-      body: success});
-    return_page.show ();
-    console.log('Servo moved to the Left!');
-  },
-  function(error) {
-    // Failure!
-    var error_page = new UI.Card ({
-      title: 'failure ',
-      body: 'oh dear :('});
-    error_page.show ();
-    console.log('Failed to move servo: OMG!! ' + error);
-  }
-);
+  var return_page = new UI.Card();
+  ajax(
+    {
+      url: 'http://192.168.1.87/?S=1',
+      type:'post'
+    },
+    function(data) 
+    { 
+      return_page.title('Up ');
+      return_page.body('\nit worked');
+      return_page.show();
+    },
+    function(error) 
+    {
+      // Failure!
+      return_page.title('Failed ' + error);
+      return_page.show();
+    }
+    );
+});
+
+main.on('click', 'down', function(e) {
+  var return_page = new UI.Card();
+  ajax(
+    {
+      url: 'http://192.168.1.87/?S=0',
+      type:'post'
+    },
+    function(data) 
+    { 
+      return_page.title('Down ');
+      return_page.body('\nit worked');
+      return_page.show();
+    },
+    function(error) 
+    {
+      // Failure!
+      return_page.title('Failed ' + error);
+      return_page.show();
+    }
+    );
 });
