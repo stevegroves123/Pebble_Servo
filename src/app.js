b4aba681537414e2dd9df6b95c4ce4d9f6201c23
Steve Groves <stevegroves123@gmail.com>
2015-02-19 16:27:51 +0000

    Updated locations
    Added locations for each button, added console.logs for error reporting

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@
 
 var UI = require('ui');
 var ajax = require('ajax');
-var url = 'http://192.168.1.87';
+// Web facing IP address for home BT system
+var url = 'http://86.186.13.138';
 var main = new UI.Card({
   title: 'Servo.js',
-  body: '\nPress any button'
+  subtitle: 'Stevie G',
+  body: '\nPress button to indicate location'
 });
 
 main.show();
@@ -23,13 +25,16 @@ main.show();
     },
     function(data) 
     { 
-      main.title ('Right ');
-      main.body ('\nit worked');
+      console.log("Servo moved to Car");
+      main.title ('Car :) ');
+      main.body ('\nNearly there');
+      
     },
     function(error) 
     {
       // Failure!
       main.title('Failed check web page');
+      console.log('Did not work - Car');
       main.hide ();
     });
 });
@@ -43,14 +48,37 @@ main.show();
     function(data) 
     { 
       // success
-      main.title('Left ');
-      main.body('\nit worked');
+      console.log("Servo moved to DMK");
+      main.title('Denmark Hill');
+      main.body('\nStarting journey');
     },
     function(error) 
     {
       // Failure!
       main.title('Something went wrong ');
-      main.body('\n  ');
+      console.log('Did not work - DMK');
+      main.hide ();
+    });
+  });
+  
+  main.on ('click','select', function(e) {
+    ajax(
+    {
+      url: url + '/?S=-1',
+      type:'post'
+    },
+    function(data) 
+    { 
+      // success
+      console.log("Servo moved to Bromley South");
+      main.title('Bromley South ');
+      main.body('\nHalf way home');
+    },
+    function(error) 
+    {
+      // Failure!
+      main.title('It\'s a trap!');
+      console.log('Did not work - Bromley South');
       main.hide ();
     });
   });
